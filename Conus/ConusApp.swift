86e277dd76d05884200ccb7a3f13c25289365ea4
Dustin Pfannenstiel <dmp@hey.com>
2021-12-11 13:03:47 -0600

    Initial Commit

---
+++ Conus/ConusApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConusApp.swift
+//  Conus
+//
+//  Created by Dustin Pfannenstiel on 12/11/21.
+//
+
+import SwiftUI
+
+@main
+struct ConusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
